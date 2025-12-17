1d9288da2dfe6c2e74995b44b2fb6efbac51d059
cuebaze <olivervalente@icloud.com>
2025-12-17 14:10:46 +0100

    Made the tabsnavigation so you can route between Dashboard, settings and add item

--- frontend/time2buyapp/app/(tabs)/_layout.tsx
+++ frontend/time2buyapp/app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
 import '../../global.css'; 
 import { Tabs } from 'expo-router';
 import {Ionicons} from '@expo/vector-icons';
+import { View } from 'react-native';
 
 export default function TabsLayout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{tabBarStyle: {backgroundColor: '#ecececff'} ,tabBarActiveTintColor: "#8ceb11ff", tabBarInactiveTintColor: '#bebebeff' }}>
             <Tabs.Screen 
                 name="index" 
                 options={{ 
@@ -18,12 +19,30 @@ export default function TabsLayout() {
                 }}
             />
             <Tabs.Screen 
-                name="about" 
+                name="item" 
+                options=
+                {{ 
+                    headerShown: false, 
+                    tabBarLabel: () => null, 
+                    tabBarIcon: ({focused}) => ( 
+                        <View className={`
+                            w-20 h-20 rounded-full items-center justify-center -mt-8 border border-4 border-[#ecececff]
+                            ${focused ? 'bg-[#8ceb11ff]' : 'bg-[#bebebeff]'}
+                        `}>
+                            <Ionicons name={focused ? "add-sharp" : "add-outline"} 
+                            color={'#fff'}
+                            size={40} /> 
+                        </View>
+                    ),
+                }} 
+            />
+            <Tabs.Screen 
+                name="settings" 
                 options=
                 {{ 
                     headerShown: false, 
                     tabBarIcon: ({focused, color}) => ( 
-                        <Ionicons name={focused ? "information-circle-sharp" : "information-circle-outline"} 
+                        <Ionicons name={focused ? "settings-sharp" : "settings-outline"} 
                         color={color} 
                         size={24} /> 
                     ),
--- frontend/time2buyapp/app/(tabs)/item.tsx
+++ frontend/time2buyapp/app/(tabs)/item.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function About() {
     return (
         <View className='flex-1 items-center justify-center bg-[#C8C8C8]'>
-            <Text className='text-3xl text-white'>This is the about section</Text>
+            <Text className='text-3xl text-white'>This is the new item page</Text>
         </View>
     );
 }
