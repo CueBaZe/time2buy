b81dba7d95d1741b56bafec218efae077fbcee23
CueBaZe <olivervalente@icloud.com>
2026-01-06 14:54:59 +0100

    Made the login function work

---
+++ backend/time2buyapi/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) { //function to check if the login infomation is correct
+        //validate the data
+        $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        //check if the login infomation is correct (user or email matches the password). If not return error message and status code (403)
+        $passwordHashed = Hash::make($request->password);
+        $user = DB::table('users')->where('name', $request->name OR 'email', $request->name)->where('password', $passwordHashed)->first();
+
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged in',
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'The provided credentials do not match our records.',
+            ], 403);
+        }
+
+        //return success message and status code
+    }
+
+    public function logout() { //function to logout the user
+        //check if the user has an active session. If not retrun error message and status code (404)
+
+        //remove the session
+
+        //return success message and status code (200)
+    }
+
+    public function register(Request $request) { //function to let the user create an account
+
+        //validate the data
+
+        //check if the username or email already exists, if it does return error message and status code (403)
+
+        //add the new account data into the user table
+
+        //return success message and status code (200)
+
+    }
+}
