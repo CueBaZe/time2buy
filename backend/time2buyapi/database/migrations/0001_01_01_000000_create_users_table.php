76b7ff29b3a29e27f92f7857d84560eb1426f3c6
CueBaZe <olivervalente@icloud.com>
2026-01-06 13:12:06 +0100

    Setup the backend to be an api project instead of a normal laravel project

--- backend/time2buyapi/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/time2buyapi/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,13 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -37,13 +30,9 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
