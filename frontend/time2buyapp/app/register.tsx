06108d38e9a7d11658896df8e206c14ad1c6ec6c
CueBaZe <olivervalente@icloud.com>
2026-01-07 09:53:45 +0100

    Made a errorBox

--- frontend/time2buyapp/app/register.tsx
+++ frontend/time2buyapp/app/register.tsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { Link } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Register() {
 
     const [isChecked, setChecked] = useState(false);
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirm, setConfirm] = useState<string>('');
+    const [errors, setErrors] = useState<string>('');
+
+    const HandleRegister = async () =>  {
+
+    }
 
     return (
         <View className='flex-1 items-center bg-[#1A1A1B]'>
@@ -14,22 +24,40 @@ export default function Register() {
                 <Text className='text-white text-lg'>See the clock behind the price tag.</Text>
             </View>
 
-            <View className='flex-1 items-center justify-center gap-[50px]'>
-                <TextInput className='text-white text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
+            <View className='mt-[25px] border bg-[#282829] p-2 rounded-2xl hidden' id='errorBox'>
+                <Text id='ErrorText' className='text-white text-md'><Ionicons name="information-circle" size={18} color="red" /></Text>
+            </View>
+
+            <View className='flex-1 items-center  justify-center gap-[50px]'>
+                <TextInput className='text-white bg-[#282829] text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
                     placeholder='Username' 
-                    id='username' 
+                    id='name'
+                    value={name}
+                    onChangeText={setName} 
                     placeholderTextColor="#ffffff">
                 </TextInput>
 
-                <TextInput className='text-white text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
+                <TextInput className='text-white bg-[#282829] text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
                     placeholder='Email' 
                     id='email' 
+                    value={email}
+                    onChangeText={setEmail} 
                     placeholderTextColor="#ffffff">
                 </TextInput>
 
-                <TextInput className='text-white text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
+                <TextInput className='text-white bg-[#282829] text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
                     placeholder='Password' 
                     id='password' 
+                    value={password}
+                    onChangeText={setPassword} 
+                    placeholderTextColor="#ffffff">
+                </TextInput>
+
+                <TextInput className='text-white bg-[#282829] text-xl text-center w-[250px] border border-2 border-[#BEBEBE] rounded-lg' 
+                    placeholder='Repeat password' 
+                    id='passwordconfirm' 
+                    value={confirm}
+                    onChangeText={setConfirm} 
                     placeholderTextColor="#ffffff">
                 </TextInput>
 
@@ -42,7 +70,7 @@ export default function Register() {
                     <Text className='text-white'>Accept <Link href={'terms'} className='underline'>Terms and Conditions</Link></Text>
                 </View>
 
-                <Text className='bg-[#8CEB11] text-white text-2xl font-bold rounded-lg p-2' id='loginBtn'>Register</Text>
+                <Text className='bg-[#8CEB11] text-white text-2xl font-bold rounded-lg p-2' id='loginBtn' onPress={HandleRegister}>Register</Text>
 
                 <View className='items-center gap-2'>
                     <Text className='text-white'>have an account?</Text>
